d7f65f0c7894ac802763ccdd06b05e38237241c6
HellLive <43405286+HellLive@users.noreply.github.com>
2020-02-18 16:10:32 -0300

    Create questao4.py

---
+++ questao4.py
@@ -0,0 +1,11 @@
+***Faça um programa que calcule o aumento de um salário. Ele deve solicitar o valor do salário e a
+porcentagem do aumento. Exiba o valor do aumento e do novo salário.***
+
+salario=int(input('Informe seu salario inicial:'))
+porcent=float(input('Informe a porcentagem:'))
+
+novo= salario*(porcent/100)
+saldo=novo+salario
+
+print('Teve um aumento no seu salario no valor de %3.2f' %novo)
+print('Seu novo salário será de %d'%saldo)
