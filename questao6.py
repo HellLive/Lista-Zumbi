53471f0201d6dc30392014bbfbdb4ba8f037d737
HellLive <43405286+HellLive@users.noreply.github.com>
2020-02-18 16:13:46 -0300

    Create questao6.py

---
+++ questao6.py
@@ -0,0 +1,9 @@
+***Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média
+esperada para a viagem.***
+
+distancia = float(input('informe a distancia a ser percorrida: '))
+velocidade= float(input('Velocidade a ser media da viagem: '))
+
+prazo =  distancia/velocidade
+
+print('O tempo aproximado de chega é %.2f minutos'%prazo)
