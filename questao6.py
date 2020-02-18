a41002be46c90367547bfcb0e4165e2eccf4dd75
HellLive <43405286+HellLive@users.noreply.github.com>
2020-02-18 16:38:29 -0300

    Update questao6.py

--- questao6.py
+++ questao6.py
@@ -1,5 +1,5 @@
-***Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média
-esperada para a viagem.***
+'''Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média
+esperada para a viagem.'''
 
 distancia = float(input('informe a distancia a ser percorrida: '))
 velocidade= float(input('Velocidade a ser media da viagem: '))
