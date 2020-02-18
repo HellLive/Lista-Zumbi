bd9685e5fc5d060a7f39bf1a9887ee3f56834313
HellLive <43405286+HellLive@users.noreply.github.com>
2020-02-18 16:14:52 -0300

    Create questao7.py

---
+++ questao7.py
@@ -0,0 +1,5 @@
+***Converta uma temperatura digitada em Celsius para Fahrenheit. F = 9*C/5 + 32***
+
+celsius=float(input('Informe a temperatura em celsius para a conversão: '))
+f=(9*celsius)/ 5+32
+print('A quantidade em Fahreinheit é de:%.1ff'%f)
