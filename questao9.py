ef41998a240ed630ff25bfdec011713e7bc14ad7
HellLive <43405286+HellLive@users.noreply.github.com>
2020-02-18 16:39:55 -0300

    Update questao9.py

--- questao9.py
+++ questao9.py
@@ -1,6 +1,6 @@
-***Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo
+'''Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo
 usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar,
-sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado.***
+sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado.'''
 
 x=int(input("Qual a quantidade de KM percorrido com o carro:"))
 y=int(input("Quantidade de dias utilizado pelo carro:"))
