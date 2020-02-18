b3519f217b4c7f7f849ff6132f44cd853cd6d11f
HellLive <43405286+HellLive@users.noreply.github.com>
2020-02-18 16:11:28 -0300

    Create questao5.py

---
+++ questao5.py
@@ -0,0 +1,11 @@
+***Solicite o preço de uma mercadoria e o percentual de desconto. Exiba o valor do desconto e o
+preço a pagar.***
+
+produto=float(input("Informe o valor do produto:"))
+desconto=int(input("Informe o valor do desconto:"))
+
+pagar=produto-(desconto/100)
+novo=desconto/100
+
+print("o valor do produto a se pagar é R$%3.2f"%pagar)
+print('o valor do desconto é R$%2.2f'%novo)
