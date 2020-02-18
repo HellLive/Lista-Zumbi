26235e5c52a9141808b8a4811a6cb0177a985a59
HellLive <43405286+HellLive@users.noreply.github.com>
2020-02-18 16:25:57 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -1,4 +1,4 @@
-***1) Faça um programa que peça dois números inteiros e imprima a soma desses dois números***
+***Faça um programa que peça dois números inteiros e imprima a soma desses dois números***
 
 y=int(input('Digite o primeiro numero a ser calculado: '))
 s=int(input('Digite o segundo numero a ser calculado: '))
