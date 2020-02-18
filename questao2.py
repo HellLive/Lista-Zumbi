c8e34d2298af30db44ec6f485fb4b25e9a2b42f6
HellLive <43405286+HellLive@users.noreply.github.com>
2020-02-18 16:07:15 -0300

    Create questao2.py

---
+++ questao2.py
@@ -0,0 +1,5 @@
+*** Escreva um programa que leia um valor em metros e o exiba convertido em milímetros***
+
+metros=int(input("quantidade de metros:"))
+milimetro=(metros*1000)
+print('A quantidades de milimetros %d' %milimetro)
