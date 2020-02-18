f704cab5aaa78393c270bfee2c57b07acce13909
HellLive <43405286+HellLive@users.noreply.github.com>
2020-02-18 16:08:44 -0300

    Create questao3.py

---
+++ questao3.py
@@ -0,0 +1,10 @@
+***Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule
+o total em segundos.***
+
+dias=int(input('Digite a quantidades de dias:'))
+horas=int(input('Digite a quantidades de horas:'))
+minutos=int(input('Digite a quantidades de dias:'))
+seg=int(input('Digite a quantidades de segundos:'))
+
+total=(dias*30*12*60*60)+(horas*60*60)+(minutos*60)+seg
+print('A quantidade em segundos %d'%total)
