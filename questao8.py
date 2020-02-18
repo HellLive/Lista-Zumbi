38554bb4d7e67125e7b1c98949d4c7e6959a1a2e
HellLive <43405286+HellLive@users.noreply.github.com>
2020-02-18 16:15:48 -0300

    Create questao8.py

---
+++ questao8.py
@@ -0,0 +1,5 @@
+***Faça agora o contrário, de Fahrenheit para Celsius.***
+
+f=float(input('Informe a temperatura em Fahrenheit para a conversão: '))
+celsius = (f-32)/1.8000
+print('A quantidade em celsius é de:%.1f'%celsius)
